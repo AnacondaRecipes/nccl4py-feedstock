83d22aa366f2374780c3e154a0160bcc153c40f5
Onur Bingol <obingol@anaconda.com>
2026-05-14 22:04:59 -0700

    update build script

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ex
 
-# conda-forge's CUDA 12+ stack places headers under a target-specific dir:
+# CUDA 12+ stack places headers under a target-specific dir:
 #   $BUILD_PREFIX/targets/<arch>-linux/include/{cuda.h, cuda_runtime_api.h, ...}
 # setup.py computes CUDA_INC as $CUDA_HOME/include, so we point CUDA_HOME at
 # that target dir. nccl.h comes from the `nccl` host dep at $PREFIX/include,
@@ -25,5 +25,5 @@ esac
 
 export CUDA_HOME="${BUILD_PREFIX}/${targetsDir}"
 
-cd nccl4py
+cd bindings/nccl4py
 $PYTHON -m pip install . --no-deps --no-build-isolation -vv
